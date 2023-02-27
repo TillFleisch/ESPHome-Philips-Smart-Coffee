8bf1790b520fd6ac4c3bd0ed015c94ec6e538a93
Till Fleisch <till@fleisch.dev>
2023-02-27 20:43:56 +0100

    Fix repeated make action issue

--- components/philips_action_button/action_button.cpp
+++ components/philips_action_button/action_button.cpp
@@ -24,60 +24,61 @@ namespace esphome
 
             void ActionButton::press_action()
             {
+                auto action = action_;
                 // Coffee
-                if (action_ == SELECT_COFFEE || action_ == MAKE_COFFEE)
+                if (action == SELECT_COFFEE || action == MAKE_COFFEE)
                 {
                     write_array({0xD5, 0x55, 0x00, 0x01, 0x02, 0x00, 0x02, 0x08, 0x00, 0x00, 0x39, 0x1C});
-                    if (action_ == SELECT_COFFEE)
+                    if (action == SELECT_COFFEE)
                         return;
 
                     delay(BUTTON_SEQUENCE_DELAY);
-                    action_ = PLAY_PAUSE;
+                    action = PLAY_PAUSE;
                 }
 
                 // Espresso
-                if (action_ == SELECT_ESPRESSO || action_ == MAKE_ESPRESSO)
+                if (action == SELECT_ESPRESSO || action == MAKE_ESPRESSO)
                 {
                     write_array({0xD5, 0x55, 0x00, 0x01, 0x02, 0x00, 0x02, 0x02, 0x00, 0x00, 0x09, 0x2D});
-                    if (action_ == SELECT_ESPRESSO)
+                    if (action == SELECT_ESPRESSO)
                         return;
                     delay(BUTTON_SEQUENCE_DELAY);
-                    action_ = PLAY_PAUSE;
+                    action = PLAY_PAUSE;
                 }
 
                 // Hot water
-                if (action_ == SELECT_HOT_WATER || action_ == MAKE_HOT_WATER)
+                if (action == SELECT_HOT_WATER || action == MAKE_HOT_WATER)
                 {
                     write_array({0xD5, 0x55, 0x00, 0x01, 0x02, 0x00, 0x02, 0x04, 0x00, 0x00, 0x21, 0x01});
-                    if (action_ == SELECT_HOT_WATER)
+                    if (action == SELECT_HOT_WATER)
                         return;
                     delay(BUTTON_SEQUENCE_DELAY);
-                    action_ = PLAY_PAUSE;
+                    action = PLAY_PAUSE;
                 }
 
                 // Steam
-                if (action_ == SELECT_STEAM || action_ == MAKE_STEAM)
+                if (action == SELECT_STEAM || action == MAKE_STEAM)
                 {
                     write_array({0xD5, 0x55, 0x00, 0x01, 0x02, 0x00, 0x02, 0x10, 0x00, 0x00, 0x09, 0x26});
-                    if (action_ == SELECT_STEAM)
+                    if (action == SELECT_STEAM)
                         return;
                     delay(BUTTON_SEQUENCE_DELAY);
-                    action_ = PLAY_PAUSE;
+                    action = PLAY_PAUSE;
                 }
 
                 // press/play or subsequent press/play
-                if (action_ == PLAY_PAUSE)
+                if (action == PLAY_PAUSE)
                     write_array({0xD5, 0x55, 0x00, 0x01, 0x02, 0x00, 0x02, 0x00, 0x00, 0x01, 0x19, 0x32});
-                else if (action_ == SELECT_BEAN)
+                else if (action == SELECT_BEAN)
                     // bean button
                     write_array({0xD5, 0x55, 0x00, 0x01, 0x02, 0x00, 0x02, 0x00, 0x02, 0x00, 0x09, 0x2F});
-                else if (action_ == SELECT_SIZE)
+                else if (action == SELECT_SIZE)
                     // size button
                     write_array({0xD5, 0x55, 0x00, 0x01, 0x02, 0x00, 0x02, 0x00, 0x04, 0x00, 0x20, 0x05});
-                else if (action_ == SELECT_AQUA_CLEAN)
+                else if (action == SELECT_AQUA_CLEAN)
                     // aqua clean button
                     write_array({0xD5, 0x55, 0x00, 0x01, 0x02, 0x00, 0x02, 0x00, 0x10, 0x00, 0x0D, 0x36});
-                else if (action_ == SELECT_CALC_CLEAN)
+                else if (action == SELECT_CALC_CLEAN)
                     // calc clean button
                     write_array({0xD5, 0x55, 0x00, 0x01, 0x02, 0x00, 0x02, 0x00, 0x20, 0x00, 0x28, 0x37});
                 else
