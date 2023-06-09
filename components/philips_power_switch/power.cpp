3a7c764209b8336599c83454afe277f0247b0cf2
davidepanato <davidepanato@users.noreply.github.com>
2023-06-09 11:19:58 +0000

    update timing

--- components/philips_power_switch/power.cpp
+++ components/philips_power_switch/power.cpp
@@ -54,7 +54,7 @@ namespace esphome
 =======
                     ESP_LOGE(TAG, "TRANSISTOR!");
                     power_pin_->digital_write(0);
-                    ESP_LOGE(TAG, "DISPLAY OFF!");
+                    ESP_LOGE(TAG, "DISPLAY OFF (1s)!");
                     delay(POWER_TRIP_DELAY);
                     power_pin_->digital_write(1);
                     ESP_LOGE(TAG, "DISPLAY ON!");
