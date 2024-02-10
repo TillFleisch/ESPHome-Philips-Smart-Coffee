b5a8ffa31169956155b101d578c1c50480738680
Till <till@fleisch.dev>
2024-02-10 18:28:49 +0100

    Restrict milk selection to cappuccino and latte

--- components/philips_coffee_machine/number/beverage_setting.cpp
+++ components/philips_coffee_machine/number/beverage_setting.cpp
@@ -34,15 +34,15 @@ namespace esphome
 
                 // only apply status if source is currently selected
                 std::string status = status_sensor_->get_raw_state();
-                if (((source_ == COFFEE || source_ == ANY) &&
+                if ((type_ != MILK && (source_ == COFFEE || source_ == ANY) &&
                      (status.compare("Coffee selected") == 0 ||
                       status.compare("2x Coffee selected") == 0 ||
                       (type_ != BEAN && status.compare("Ground Coffee selected") == 0))) ||
-                    ((source_ == ESPRESSO || source_ == ANY) &&
+                    (type_ != MILK && (source_ == ESPRESSO || source_ == ANY) &&
                      (status.compare("Espresso selected") == 0 ||
                       status.compare("2x Espresso selected") == 0 ||
                       (type_ != BEAN && status.compare("Ground Espresso selected") == 0))) ||
-                    ((source_ == AMERICANO || source_ == ANY) &&
+                    (type_ != MILK && (source_ == AMERICANO || source_ == ANY) &&
                      (status.compare("Americano selected") == 0 ||
                       status.compare("2x Americano selected") == 0 ||
                       (type_ != BEAN && status.compare("Ground Americano selected") == 0))) ||
@@ -52,7 +52,7 @@ namespace esphome
                     ((source_ == LATTE_MACCHIATO || source_ == ANY) &&
                      (status.compare("Latte Macchiato selected") == 0 ||
                       (type_ != BEAN && status.compare("Ground Latte Macchiato selected") == 0))) ||
-                    (type_ != BEAN && (source_ == HOT_WATER || source_ == ANY) &&
+                    (type_ != BEAN && type_ != MILK && (source_ == HOT_WATER || source_ == ANY) &&
                      status.compare("Hot water selected") == 0))
                 {
                     uint8_t enable_byte = type_ == BEAN ? 9 : 11;
